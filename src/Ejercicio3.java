6a58bc2ab3aea714fc555ca799d4597012562a3a
LAPTOP-UR5401LD\MaryAng <maryangrb@gmail.com>
2020-06-29 20:56:04 -0300

    Hice el ej3 en Intell Idea, y por aquí voy a hacer el commit.

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
+    public static void main (String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int num = input.nextInt();
+
+        int duplicar = calcularDoble (num);
+        System.out.println("El doble del número es " + duplicar);
+    }
+
+    public static int calcularDoble (int num){
+        return num * 2;
+    }
 }
